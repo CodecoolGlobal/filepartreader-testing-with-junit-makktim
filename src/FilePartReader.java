c4cbbf1dbd6252156abb6f1507bd7cfd2342d4f1
makktim <kasatimi01@gmail.com>
2019-07-17 17:28:52 +0200

    filePartReader and fileWordAnalyzer is done

--- src/FilePartReader.java
+++ src/FilePartReader.java
@@ -1,45 +1,56 @@
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
 import java.nio.file.Files;
-import java.util.Scanner;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 public class FilePartReader  {
 
 
-//    private String filePath = ("/home/timi/Codecool/3oop/5_si_week/filePractice/src/practice.txt");
+    //    private String filePath = ("/home/timi/Codecool/3oop/5_si_week/filePractice/src/practice.txt");
     private String filePath;
     private Integer fromLine;
     private Integer toLine;
 
 
     public FilePartReader() {
+//        this.filePath = "";
 
     }
 
 
     public void setup(String filePath, Integer fromLine, Integer toLine ) throws IllegalArgumentException{
+
         if(toLine < fromLine || fromLine < 1){
             throw new IllegalArgumentException();
         }
+
         this.filePath = filePath;
         this.fromLine = fromLine;
         this.toLine = toLine;
     }
 
-    public String read(){
-        return new String(Files.readAllBytes(paths.get(filePath)));
+    public String read() throws IOException {
+        String data = "";
+        data = new String(Files.readAllBytes(Paths.get(filePath)));
+        return data;
 
     }
 
     public String readLines() {
-      String readTheLines = "";
-      try{
-          readTheLines = read();
-      }catch (IOException e){
-          e.printStackTrace();
-      }
-      Bufferedreader bufferedreader = new BufferedReader(new StringReader(readTheLines));
-      Stream<string> printedLines = bufferedreader.lines().skip(fromLine).limit(toLine);
-      return printedLines.collect(Collectors.joining("\n"));
+        String readTheLines="";
+
+        try {
+            readTheLines = read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        BufferedReader bufferedReader = new BufferedReader(new StringReader(readTheLines));
+        Stream<String> printedLines = bufferedReader.lines().skip(fromLine-1).limit(toLine-1);
+
+        return printedLines.collect(Collectors.joining("\n"));
     }
 }
--- src/FileWordAnalyzer.java
+++ src/FileWordAnalyzer.java
@@ -1,23 +1,61 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class FileWordAnalyzer {
 
+    private final FilePartReader filePartReader;
 
-    public FileWordAnalyzer(FilePartReader FilePartReader){
-
+    public FileWordAnalyzer(FilePartReader filePartReader) {
+        this.filePartReader = filePartReader;
     }
 
-    public List getWordsOrderedAlphabetically(){
+    public List getWordsOrderedAlphabetically (){
+        String readLines = filePartReader.readLines().replaceAll("\n", " ");
+        String[] words = readLines.split(" ");
+        Arrays.sort(words);
 
-        return null;
+        return Arrays.asList(words);
     }
 
-    public List getWordsContainingSubstring(String subString ){
-        return null;
+    public List getWordsContainingSubstring (String subString) {
+        String text = filePartReader.readLines();
+        String lines[] = text.split("\n");
+        List<String> words = new ArrayList<>();
 
+        for (int i = 0; i < lines.length; i++) {
+            String[] strings = lines[i].split(" ");
+            for (String word : strings) {
+                if (!word.equals(" ") && !word.isEmpty()) {
+                    if (word.contains(subString)) {
+                        words.add(word);
+                    }
+                }
+            }
+        }
+        return words;
     }
 
-    public List getStringsWhichPalindromes(){
-        return null;
+    public List getStringsWhichPalindrome() {
+        String lineBreaks = filePartReader.readLines().replaceAll("\n", " ");
+        String[] palindromeWords = lineBreaks.split(" ");
+
+        StringBuilder result = new StringBuilder();
+        StringBuilder wordsReverse = new StringBuilder();
+
+
+        for (String word : palindromeWords) {
+            wordsReverse.append(word).reverse();
+            if (wordsReverse.toString().equals(word)) {
+                result.append(word + " ");
+            }
+            wordsReverse.delete(0, wordsReverse.length());
+        }
+
+        if (result.toString().length() == 0) {
+            return null;
+        } else {
+            return Arrays.asList(result.toString().split(" "));
+        }
     }
 }
